bc5fccdf9b995ddf308455bdc036be8e4a7b95d3
LEONARDO ROJAS <LEONARDO ROJAS@DESKTOP-M5C1M8A>
2025-01-21 21:26:24 -0500

    carga inicial

--- src/proyecto1_clase4/Proyecto1_clase4.java
+++ src/proyecto1_clase4/Proyecto1_clase4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Proyecto1_clase4 {
     public static void main(String[] args) {
     // declarar variables
-    int n1,n2,n3, suma;
+    int n1,n2,n3,n4, suma;
     Scanner lectura = new Scanner(System.in);
     //Entrada de datos
     System.out.print("Ingresar Numero 1: ");
@@ -13,8 +13,10 @@ public class Proyecto1_clase4 {
     n2=lectura.nextInt();
     System.out.print("Ingresar Numero 3: ");
     n3=lectura.nextInt();
+    System.out.print("Ingresar Numero 4: ");
+    n4=lectura.nextInt();
     // procesar datos
-    suma=n1+n2+n3;
+    suma=n1+n2+n3+n4;
     // salida de datos
     System.out.println("La suma de los numeros es: " + suma);
 }
